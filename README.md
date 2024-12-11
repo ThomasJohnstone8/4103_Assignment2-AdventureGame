4dad377c83c4942781b8c070467fd327205fe443
ThomasJohnstone8 <2407312@leedstrinity.ac.uk>
2024-12-11 17:32:57 +0000

    updated readme and completed the program

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 # 4103_Assignment2-AdventureGame
 
 
-
 # Introduction
 
-introduction text
+In this assignment I aim to make an adventure game where 2 player face each other in a 5x5 grid trying to reach the treasure. The treasure is randomly placed in the 5x5 grid and both player start at (0,0), the top left corner of the grid. Additionally, there are traps in the grid that the player must avoid because if the player hits too many traps then the will continue to loose health until they die. Once the player is dead the other playe automatically wins. There are also power ups in the grid that allow the player to gain health or a hint to where the treasure is. Lastly, there is obstacles that the player cannot interact with and their purposse in the grid is to diversify the route to the treasure.
 
-
-learning outcomes, code readability and maintainbility??? again?
 
 # Project Overview
 
-discuss methodlogies used (scrum, kanban, waterfall, etc)
+During this project I used a Trello board to track my progress. Trello boards use the Kanban methodology. This is a scrum methodolgy which its main ideology is having 3 columns that you follow; To Do & Backlog, In progress and Done Tasks. With these 3 key concept I was able to visualise and see my progress being made as I was working on the project.
 
-kanban: TRELLO :O
+Other methodlogies I considered was Waterfall. The waterfall methodology has you following a strict, linear step-by-step process that is easy to grasp. The reason I picked Kanban over waterfall is because I wanted the freedom to return to my backlog of tasks and do multiple parts of my project at once, so if i got stuck somewhere, I can shift my focus to one of my other tasks that are in my in progress column. This differs from waterfall where you follow a linear waterfall-like method that paths down.
+
+Another methodology I decided not to use was scrum. Scrum focuses on small workloads in manageble chunks. These tasks are then followed in sprints. These sprints are quick hense the name "sprints" and can vary in time. Usually these times are discussed in the collaberation section of the methodology however, this is a solo project and therefore I would set them myself if i were to use scrum.
 
 
 # Challenges and Solutions 
 
-What are challenges faced in the making of the project? How were these solved?
+Some challenges I faced when making this project was the lack of time to collaberate with my tutor. This project was started during the first semester however, finished in the winter holidays meaning there was little to ask and collaberate with the tutor. In addition to this, another challenge I faced was migrating to a laptop from the virtual machine, this was quite easy but still took me some time to transfer files and more. Another challenges I dealt with was the searching algorithms. A lot of the coding of the functions and main gameplay loop were familiar after experiencing the lab activity and just a general increase in my knowledge of python. However, the searching algorithms were difficult and the way I solved it was I spent a lot of time revising notes and refering to Moodle documents for help understanding it. Another challenge I faced was some of the validation. At first my game would let the player move outside of the grid and the game would end. This was because of the validation I made that if only 1 person is in the grid the game ended. I then had to introduce validation so that only moves inside the grid were permitted and if the player tried to exit the grid, the questions would repeat in a while loop until a valid move was made.
 
 
 # Search Algorithms
@@ -37,7 +36,23 @@ Psuedocode?
 
 # Testing Evidence
 
-Test number, Test Data, Reason, Expected outcome, actual outcome, pass/fail?
+| Test No. | Test Data | Expected Outcome | Actual Outcome | Pass/Fail? |
+|:--------:|-----------|------------------|----------------|------------|
+| 1 | What will happen when the player moves outside of the grid? | An appropriate error message is displayed | An error message stating "Invalid move. Please stay within grid boundaries. Try again." was displayed | Pass |
+| 2 | What will happen when a trap is stepped on? | When a trapped is stepped on, the player will lose 2 health | The player lost 2 health and the counter went from the starting 10 down to 8 | Pass |
+| 3 | What will happen when the user changes there mind and wants to change their move? | The player will an extra option for the player to return to the previous question | There is no option to return and change your move, once a move has been selected, the player must complete their turn | Fail |
+| 4 | What happens when the user reaches the treasure? | The player will win and the game will end and display a congratulation message | The game only ends after the next player has completed their turn, this could be due to the gameplay loop however, the game still registers that the user is ontop of the treasure and has won the game | Fail |
+| 5 | What will happen when the player health reaches zero? | The game will recognise that a player has died and only 1 player is left and end the game | Similar to the treasure test, the game only recognises that the player has died after the next players turn has concluded | Fail |
+| 6 | What will happen when a player tries to move into an obstacle | The game will recognise the obstacle and make the player take their turn again | The player didnt move to the obstacle and was made to take their turn again and also displayed this error message "Invalid move. You can't move into an obstacle or another player. Try again." | Pass |
+| 7 | What will happen when a player tries to move ontop of another player | The game will recognise a player is already taking up the square and will make the player take their turn again | Similar to the obstacle being hit, the player is make to retake their turn and an appropriate error message is displayed "Invalid move. You can't move into an obstacle or another player. Try again." | Pass |
+| 8 | When the game starts is a 5x5 grid generated? | The grid is generate and have empty spaces, Power ups (P), Traps (X) and Obstacles (O) | The grid is generated with all relevent pieces randomly placed inside | Pass |
+| 9 | When the game starts how many treasures are on the grid? | I expect there to be only 1 treasure on the grid | Only 1 treasure is on the grid at a time | Pass |
+| 10 | Are the players are both starting at (0,0)? | The expected result is that the players both start at (0,0) in the grid | Both players start at the same point in the grid being (0,0) | Pass |
+| 11 | Does the BFS give the player to shortest path to the treasure whilst also ensuring they dont die getting there? | Yes the BFS gives the shortest path to the player with directions aswell | The BFS gives the shortest route to the player and also displays it in directions (up, down, left, right) | Pass |
+| 12 | If the user enters the number 5 (which isnt an option), what will happen? | I think that the program will give an error message and repeat the question again | An error message appeared and the program repeated the question again which loops until a correct answer is given (1-4) | Pass |
+| 13 | What happens when a player picks up a health power up when at 10 health points? | I expect the player to have 12 health and exceed the 10 they start with, thus giving the player an overheal | The player was given the extra 2 health and the health total was now 12 | Pass |
+| 14 | What happens when the hint power up is picked up and the treasure is not nearby at all? | The hint will tell the user that the treasure is not nearby is infact, quiet a distance away | The power up hint will always say that the treasure is nearby even if it is not | Fail |
+| 15 | Does the Binary search allow the user to search certain row or columns to narrow the search for the treasure? | I expect the Binary search to allow the player to search rows and columns to try find the treasure | The Binary search does allow the user to search for the treasure however, I am unsure how nessecary this is because the BFS directs the player straight to the treasure | Fail |
 
 
 # Conclusions
@@ -47,7 +62,7 @@ sum up how the project went. shortly mention the challenges and solutions
 
 # References
 
-text
+Refered to the Moodle documentation for the searching algorithms.
 
 
 
---
+++ adventureGame.py
@@ -0,0 +1,255 @@
+import random
+from collections import deque
+
+# Grid size and starting health of the player
+gridSize = 5
+startHealth = 10
+
+# List of tuples corrosponding to the direction 
+directionOptions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # (up, down, left, right)
+directionName = ["Up", "Down", "Left", "Right"]
+
+# Create the grid in terminal
+def generateGrid(players):
+    grid = [['.' for _ in range(gridSize)] for _ in range(gridSize)] # Making every point in the grid a [.]
+
+    # Place treasure
+    treasure_x, treasure_y = random.randint(0, gridSize-1), random.randint(0, gridSize-1) # Use random module to randomly place the treasure
+    grid[treasure_x][treasure_y] = 'T'
+
+    # Place traps and obstacles randomly
+    for _ in range(gridSize * 2):  # Randomly place traps and obstacles * 2 so there are more traps and obstacles
+        x, y = random.randint(0, gridSize-1), random.randint(0, gridSize-1)
+        if grid[x][y] == '.':
+            grid[x][y] = random.choice(['X', 'O'])
+
+    # Place power-ups
+    for _ in range(gridSize):  # Randomly place power-ups
+        x, y = random.randint(0, gridSize-1), random.randint(0, gridSize-1)
+        if grid[x][y] == '.':
+            grid[x][y] = 'P'
+
+    # Place both players at the same starting position (0, 0)
+    for player in players:
+        player.position = (0, 0)
+
+    return grid, (treasure_x, treasure_y)
+
+# Display the grid
+def displayGrid(grid, players):
+    print("\nGrid:")
+    for r in range(gridSize):
+        row = ""
+        for c in range(gridSize):
+            cell = grid[r][c]
+            # Display player positions
+            player_here = [player for player in players if player.position == (r, c)] # Checks what the players position is in the grid
+            if player_here:
+                row += f"[{player_here[0].name}] "  # Display player name
+            else:
+                row += f"[{cell}] " # Formatted string to display the cells ([.], T, X, O, P)
+        print(row)
+    print()
+
+# Player class
+class Player:
+    def __init__(self, name):
+        self.name = name  # Name is 1 or 2
+        self.health = startHealth
+        self.position = None  # Player position will be set during grid generation
+
+# Search Algorithms
+
+
+# BFS: Find shortest path to treasure with movement directions
+def bfs(grid, start, goal):
+    visited = [[False] * gridSize for _ in range(gridSize)]
+    queue = deque([(start, [])])  # (current_position, path_taken)
+    directionMap = {
+        (-1, 0): "Up",
+        (1, 0): "Down",
+        (0, -1): "Left",
+        (0, 1): "Right"
+    } # Dictionary that changes the directional deltas into directions a player can read easier
+    
+    while queue:
+        (x, y), path = queue.popleft()
+        
+        # If we reach the goal, return the path with directions
+        if (x, y) == goal:
+            directions = []
+            for move in path:
+                directions.append(directionMap[move])
+            return directions
+        
+        for dx, dy in directionOptions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < gridSize and 0 <= ny < gridSize and not visited[nx][ny]:
+                if grid[nx][ny] not in ['O']:  # Can’t move through obstacles
+                    visited[nx][ny] = True
+                    queue.append(((nx, ny), path + [(dx, dy)]))
+    
+    return []  # Return an empty list if no valid path was found
+
+
+# DFS: Explore deeply
+def dfs(grid, start):
+    stack = [start]
+    visited = set()
+    while stack:
+        x, y = stack.pop()
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+        for dx, dy in directionOptions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < gridSize and 0 <= ny < gridSize:
+                if grid[nx][ny] not in ['O']:  # Can’t move through obstacles
+                    stack.append((nx, ny))
+    return visited
+
+# Binary Search (BS): Search a specific row or column for treasure or power-ups
+def binarySearch(grid, searchType, target):
+    if searchType == "row":
+        # Search in each row for the target (treasure or power-up)
+        for r in range(gridSize):
+            row = [grid[r][c] for c in range(gridSize)]
+            low, high = 0, gridSize - 1
+            while low <= high:
+                mid = (low + high) // 2
+                if row[mid] == target:
+                    return r, mid  # Found the target in this row
+                elif row[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+    elif searchType == "column":
+        # Search in each column for the target (treasure or power-up)
+        for c in range(gridSize):
+            column = [grid[r][c] for r in range(gridSize)]
+            low, high = 0, gridSize - 1
+            while low <= high:
+                mid = (low + high) // 2
+                if column[mid] == target:
+                    return mid, c  # Found the target in this column
+                elif column[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+    return None  # Target not found
+
+# Main game loop
+def main():
+    # Initialize players as Player 1 and Player 2
+    players = [Player("1"), Player("2")]  # Name players as 1 and 2
+    # Generate grid with players and treasure
+    grid, treasurePosition = generateGrid(players)
+    
+    currentPlayerIndex = 0 # 0 is 1 and 1 is 2 because python counting starts at 0
+    while True:
+        player = players[currentPlayerIndex]
+        # Display grid and player info
+        displayGrid(grid, players)
+        print(f"Player {player.name}'s Health: {player.health}")
+
+        # Every loop it checks if the player has won 
+        if player.position == treasurePosition:
+            print(f"Player {player.name} found the treasure! They win!")
+            break  # Exit the loop and end the game
+        
+        # Always checks what the player health is and if it's 0, the game end
+        if player.health <= 0:
+            print(f"Player {player.name} has been eliminated!")
+            players.remove(player) # Also removes the player when eliminated
+
+            # If the length of players is  1, the game ends
+            if len(players) == 1:
+                print(f"Player {players[0].name} wins!")
+                break  # Exit the loop and end the game
+        
+        # Player's options each turn
+        print("Your turn. What would you like to do?")
+        print("1. Move (Up, Down, Left, Right)")
+        print("2. Use BFS to find the shortest path to the treasure")
+        print("3. Use DFS to explore the grid")
+        print("4. Use Binary Search to search a row or column for treasure/power-up")
+
+        choice = input("Enter your choice (1/2/3/4): ").strip()
+
+        if choice == "1":
+            # Move player (loop to retry if invalid move)
+            while True:
+                print("Move in which direction?")
+                for directionIndex, direction in enumerate(directionName):
+                    print(f"{directionIndex + 1}. {direction}")
+                directionChoice = int(input("Enter choice (1-4): ")) - 1
+
+                # Validates that it is only within the 4 options.
+                if directionChoice not in range(4):
+                    print("Invalid move. Try again.")
+                    continue
+                dx, dy = directionOptions[directionChoice]
+                newPosition = (player.position[0] + dx, player.position[1] + dy)
+
+                # Validate move (stay within grid boundaries and not overlap another player)
+                if 0 <= newPosition[0] < gridSize and 0 <= newPosition[1] < gridSize:
+                    # Check if the new position is empty and not an obstacle
+                    if newPosition not in [p.position for p in players] and grid[newPosition[0]][newPosition[1]] != 'O':
+                        player.position = newPosition
+                        # Check for traps or power-ups
+                        if grid[player.position[0]][player.position[1]] == 'X':
+                            player.health -= 2  # Trap hit
+                            print(f"Player {player.name} stepped on a trap and lost 2 health.")
+                        elif grid[player.position[0]][player.position[1]] == 'P':
+                            powerType = random.choice(['health', 'hint'])
+                            if powerType == 'health':
+                                player.health += 2  # Restore health
+                                print(f"Player {player.name} found a power-up and gained 2 health.")
+                            elif powerType == 'hint':
+                                print(f"Player {player.name} found a power-up: The treasure is nearby!")
+
+                        break  # Exit loop after valid move
+                    else:
+                        print("Invalid move. You can't move into an obstacle or another player. Try again.")
+                else:
+                    print("Invalid move. Please stay within grid boundaries. Try again.")
+
+        elif choice == "2":
+            # BFS to find shortest path to treasure
+            path = bfs(grid, player.position, treasurePosition)
+            if path:
+                print(f"Shortest path to the treasure: {path}")
+            else:
+                print("No valid path found to the treasure.")
+        
+        elif choice == "3":
+            # DFS to explore deeper
+            explored = dfs(grid, player.position)
+            print(f"Explored cells: {explored}")
+
+        elif choice == "4":
+            # Binary Search to find treasure or power-ups in a certain row or column
+            searchType = input("Search in row or column? (Enter 'row' or 'column'): ").strip().lower()
+            if searchType not in ['row', 'column']:
+                print("Invalid choice. Please enter 'row' or 'column'.")
+                continue
+            target = input("Enter target to search for ('T' for treasure, 'P' for power-up): ").strip().upper()
+            if target not in ['T', 'P']:
+                print("Invalid target. Please enter 'T' for treasure or 'P' for power-up.")
+                continue
+            result = binarySearch(grid, searchType, target)
+            if result:
+                print(f"Found {target} at position {result}")
+            else:
+                print(f"{target} not found in the {searchType}.")
+        
+        else:
+            print("Invalid choice, try again.")
+            continue
+        
+        # Switches between players 1 and 2
+        currentPlayerIndex = 1 - currentPlayerIndex
+
+# Run the game
+if __name__ == "__main__":
+    main()
